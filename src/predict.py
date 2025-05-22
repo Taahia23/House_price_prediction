54b64b65936d6a788db240ab528679b0ff237f0d
Taahia23 <taahiatahsin@gmail.com>
2025-05-22 23:35:20 +0600

    separated features

--- src/predict.py
+++ src/predict.py
@@ -2,3 +2,15 @@ import joblib
 import pandas as pd
 
 model = joblib.load('models/model_with_pipeline.pkl')  # or 'models/mlp_model.pkl'
+
+
+# Load the Test Data
+test_data = pd.read_csv('data/test_set.csv')
+
+# Separate Features (X_test) and Target (y_test)
+'''
+X_test contains all input features (independent variables)
+y_test contains the actual house prices (dependent variable)
+'''
+X_test = test_data.drop('House_Price', axis=1)
+y_test = test_data['House_Price'].copy()
