1a7103b0ccd2ab46abbc7725db26f5e88b27c992
Taahia23 <taahiatahsin@gmail.com>
2025-05-24 00:14:34 +0600

    load the trained model

--- app/app.py
+++ app/app.py
@@ -5,3 +5,11 @@ import pandas as pd
 # Set up the app
 st.set_page_config(page_title="Simple House Price Predictor", layout="centered")
 st.title("Simple House Price Predictor")
+
+
+# Load your trained model
+@st.cache_data
+def load_model():
+    return joblib.load('models/model_with_pipeline.pkl')
+
+model = load_model()
