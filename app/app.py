04b4757ac3a028e16831c693507d332479b3f563
Taahia23 <taahiatahsin@gmail.com>
2025-05-24 00:14:03 +0600

    set up the app

---
+++ app/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+# Set up the app
+st.set_page_config(page_title="Simple House Price Predictor", layout="centered")
+st.title("Simple House Price Predictor")
