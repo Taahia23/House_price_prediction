1188323bbafd0be3fcba1d25dd2bdc78757112e2
Taahia23 <taahiatahsin@gmail.com>
2025-05-25 01:14:38 +0600

    streamlit app.py code updated

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-# Set up the app
+
 st.set_page_config(page_title="Simple House Price Predictor", layout="centered")
 st.title("Simple House Price Predictor")
 
@@ -10,7 +10,7 @@ st.title("Simple House Price Predictor")
 # Load your trained model
 @st.cache_data
 def load_model():
-    return joblib.load('models/model_with_pipeline.pkl')
+    return joblib.load('src/models/model_with_pipeline.pkl')
 
 model = load_model()
 
