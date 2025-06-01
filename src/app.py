9ee277e113b304975d72b7f8ec21ed59088c7af7
Taahia23 <taahiatahsin@gmail.com>
2025-06-01 22:48:53 +0600

    updated lot_size

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ sample_data = {
     'Num_Bedrooms': 3,
     'Num_Bathrooms': 2,
     'Year_Built': 1995,
-    'Lot_Size': 0.5,
+    'Lot_Size': 0.6,
     'Garage_Size': 1,
     'Neighborhood_Quality': 7
 }
